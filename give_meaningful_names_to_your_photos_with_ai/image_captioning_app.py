145ba46cef9dc6b2f18a798c785fb02aeffe9db5
OlivierR63 <olivier.rochat@gmail.com>
2025-08-13 03:15:40 -0400

    Complete file image_captioning_app.py

--- give_meaningful_names_to_your_photos_with_ai/image_captioning_app.py
+++ give_meaningful_names_to_your_photos_with_ai/image_captioning_app.py
@@ -3,24 +3,26 @@ import numpy as np
 from PIL import Image
 from transformers import AutoProcessor, BlipForConditionalGeneration
 
-processor = # write your code here
-model = # write your code here
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 def caption_image(input_image: np.ndarray):
     # Convert numpy array to PIL Image and convert to RGB
     raw_image = Image.fromarray(input_image).convert('RGB')
-    
+
     # Process the image
-    
+    inputs = processor(raw_image, return_tensors="pt")
 
     # Generate a caption for the image
+    out = model.generate(**inputs,max_length=50)
 
-
-    # Decode the generated tokens to text and store it into `caption`
-    
+    # Decode the generated tokens to text
+    caption = processor.decode(out[0], skip_special_tokens=True)
 
     return caption
 
+# Create the web app (gradio) interface
 iface = gr.Interface(
     fn=caption_image, 
     inputs=gr.Image(), 
@@ -28,3 +30,6 @@ iface = gr.Interface(
     title="Image Captioning",
     description="This is a simple web app for generating captions for images using a trained model."
 )
+
+# Launch the web app interface
+iface.launch()
