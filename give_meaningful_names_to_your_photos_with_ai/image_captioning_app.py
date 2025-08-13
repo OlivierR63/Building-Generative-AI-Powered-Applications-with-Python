b3173b09e65357a011f8accfd619106a937f3882
OlivierR63 <olivier.rochat@gmail.com>
2025-08-13 03:28:03 -0400

    Fix typos for flake8 linting purpose

--- give_meaningful_names_to_your_photos_with_ai/image_captioning_app.py
+++ give_meaningful_names_to_your_photos_with_ai/image_captioning_app.py
@@ -31,9 +31,11 @@ iface = gr.Interface(
     inputs=gr.Image(),
     outputs="text",
     title="Image Captioning",
-    description=("This is a simple web app "
-                    "for generating captions for images "
-                    "using a trained model.")
+    description=(
+        "This is a simple web app "
+        "for generating captions for images "
+        "using a trained model."
+        )
 )
 
 # Launch the web app interface
