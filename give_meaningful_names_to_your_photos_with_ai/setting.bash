9925143b78da63fff2c89788315b24cf8940ef6c
OlivierR63 <olivier.rochat@gmail.com>
2025-08-13 03:33:22 -0400

    Update the file setting.bash

--- give_meaningful_names_to_your_photos_with_ai/setting.bash
+++ give_meaningful_names_to_your_photos_with_ai/setting.bash
@@ -5,7 +5,7 @@ pip3 install virtualenv
 virtualenv my_env
 
 # activate my_env
-source my_env/bin/activate
+source ./my_env/bin/activate
 
 # installing required libraries in my_env
 pip install langchain==0.1.11 gradio==5.23.2 transformers==4.38.2 bs4==0.0.2 requests==2.31.0 torch==2.2.1
