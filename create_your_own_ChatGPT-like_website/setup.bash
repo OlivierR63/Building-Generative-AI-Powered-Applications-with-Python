709cb6844b4beaee0c2f09b8ddfc91e9203217e0
OlivierR63 <olivier.rochat@gmail.com>
2025-08-16 10:17:36 -0400

    Update setup files

---
+++ create_your_own_ChatGPT-like_website/setup.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ../setup.bash
+python3 -m pip install transformers==4.30.2 torch
--- image_captioning_with_generative_ai/setup.bash
+++ image_captioning_with_generative_ai/setup.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
-pip3 install virtualenv
 
-# create a virtual environment my_env
-virtualenv my_env
-
-# activate my_env
-source ./my_env/bin/activate
+source ../setup.bash
 
 # installing required libraries in my_env
 pip install langchain==0.1.11 gradio==5.23.2 transformers==4.38.2 bs4==0.0.2 requests==2.31.0 torch==2.2.1
