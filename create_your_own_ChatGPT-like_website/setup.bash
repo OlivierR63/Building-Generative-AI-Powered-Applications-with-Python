7597f8caf6ba9f862afd6591a3c891b2e66ff254
OlivierR63 <olivier.rochat@gmail.com>
2025-08-16 11:11:24 -0400

    Update folder create_your_own_ChatGPT-like_website

--- create_your_own_ChatGPT-like_website/setup.bash
+++ create_your_own_ChatGPT-like_website/setup.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 source ../setup.bash
-python3 -m pip install transformers==4.30.2 torch
+
+
+python3.11 -m pip install flask
+python3.11 -m pip install flask_cors
+
+python3.11 -m pip install transformers==4.38.2 torch==2.2.1
