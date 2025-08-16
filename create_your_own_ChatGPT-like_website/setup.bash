5229ed2de9c3491ba51326c217abad9ccccc2696
OlivierR63 <olivier.rochat@gmail.com>
2025-08-16 12:03:34 -0400

    Update file setup.bash in the folder create_your_own_ChatGPT-like_website

--- create_your_own_ChatGPT-like_website/setup.bash
+++ create_your_own_ChatGPT-like_website/setup.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 source ../setup.bash
 
+echo "\nInstall flask\n"
 python3 -m pip install flask
+
+echo "\nInstall flask_cors\n"
 python3 -m pip install flask_cors
 
+echo "\nInstall transformers and torch\n"
 python3 -m pip install transformers torch
+
+echo "\n Install packages enumerated in LLM_application_chatbot/requirements.txt\n"
+python3 -m pip install -r LLM_application_chatbot/requirements.txt
