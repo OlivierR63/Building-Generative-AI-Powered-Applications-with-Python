49debb27c75b17bf72c4948cbdce384a95f21b37
OlivierR63 <olivier.rochat@gmail.com>
2025-08-17 11:20:03 -0400

    Update file script.js

--- create_your_own_ChatGPT-like_website/LLM_application_chatbot/static/script.js
+++ create_your_own_ChatGPT-like_website/LLM_application_chatbot/static/script.js
@@ -9,96 +9,106 @@ const messageInput = document.getElementById('message-input');
 
 // Section: function to create the dialogue window
 const addMessage = (message, role, imgSrc) => {
-  // create elements in the dialogue window
-  const messageElement = document.createElement('div');
-  const textElement = document.createElement('p');
-  messageElement.className = `message ${role}`;
-  const imgElement = document.createElement('img');
-  imgElement.src = `${imgSrc}`;
+    // create elements in the dialogue window
+    const messageElement = document.createElement('div');
+    const textElement = document.createElement('p');
+    messageElement.className = `message ${role}`;
+    const imgElement = document.createElement('img');
+    imgElement.src = `${imgSrc}`;
 
-  // Append the image and message to the message element
-  messageElement.appendChild(imgElement);
-  textElement.innerText = message;
-  messageElement.appendChild(textElement);
-  messagesContainer.appendChild(messageElement);
+    // Append the image and message to the message element
+    messageElement.appendChild(imgElement);
+    textElement.innerText = message;
+    messageElement.appendChild(textElement);
+    messagesContainer.appendChild(messageElement);
 
-  // Create the ending of the message
-  var clearDiv = document.createElement("div");
-  clearDiv.style.clear = "both";
-  messagesContainer.appendChild(clearDiv);
+    // Create the ending of the message
+    var clearDiv = document.createElement("div");
+    clearDiv.style.clear = "both";
+    messagesContainer.appendChild(clearDiv);
 };
-//
 
 
 // Section: Calling the model
 const sendMessage = async (message) => {
-  addMessage(message, 'user','../static/user.jpeg');
-  // Loading animation
-  const loadingElement = document.createElement('div');
-  const loadingtextElement = document.createElement('p');
-  loadingElement.className = `loading-animation`;
-  loadingtextElement.className = `loading-text`;
-  loadingtextElement.innerText = 'Loading....Please wait';
-  messagesContainer.appendChild(loadingElement);
-  messagesContainer.appendChild(loadingtextElement);
+    addMessage(message, 'user','../static/user.jpeg');
+    // Loading animation
+    const loadingElement = document.createElement('div');
+    const loadingtextElement = document.createElement('p');
+    loadingElement.className = `loading-animation`;
+    loadingtextElement.className = `loading-text`;
+    loadingtextElement.innerText = 'Loading....Please wait';
+    messagesContainer.appendChild(loadingElement);
+    messagesContainer.appendChild(loadingtextElement);
 
-  async function makePostRequest(msg) {
-    const url = 'https://olivierrocha-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/chatbot'
-    const requestBody = {
-      prompt: msg
-    };
+    async function makePostRequest(msg) {
+        const url = 'https://olivierrocha-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/chatbot'
+        const requestBody = {
+        prompt: msg
+        };
   
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-      });
-  
-      const data = await response.text();
-      // Handle the response data here
-      console.log(data);
-      return data;
-    } catch (error) {
-      // Handle any errors that occurred during the request
-      console.error('Error:', error);
-      return error
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            });
+        
+            const data = await response.json();
+
+            // Deleting the loading animation
+            const loadanimation = document.querySelector('.loading-animation');
+            const loadtxt = document.querySelector('.loading-text');
+            loadanimation.remove();
+            loadtxt.remove();
+
+            if (data.error) {
+                // Handle the error here
+                const errorMessage = JSON.stringify(data);
+                addMessage(errorMessage, 'error', '../static/Error.png');
+            }
+            else {
+                // Process the normal response here
+                const responseMessage = data.response;
+                addMessage(responseMessage, 'aibot', '../static/Bot_logo.png');
+            }
+        }
+        catch (error) {
+            // Handle any errors that occurred during the request
+            console.error('Error:', error);
+            return error
+        }
     }
-  }
   
-  var res = await makePostRequest(message);
-  
-  data = {"response": res};
-  
-  // Deleting the loading animation
-  const loadanimation = document.querySelector('.loading-animation');
-  const loadtxt = document.querySelector('.loading-text');
-  loadanimation.remove();
-  loadtxt.remove();
+    var res = await makePostRequest(message);
+    
+    data = {"response": res};
+    
+    // Deleting the loading animation
+    const loadanimation = document.querySelector('.loading-animation');
+    const loadtxt = document.querySelector('.loading-text');
+    loadanimation.remove();
+    loadtxt.remove();
 
-  if (data.error) {
-    // Handle the error here
-    const errorMessage = JSON.stringify(data);
-    addMessage(errorMessage, 'error','../static/Error.png');
-  } else {
-    // Process the normal response here
-    const responseMessage = data['response'];
-    addMessage(responseMessage, 'aibot','../static/Bot_logo.png');
-  }
-  
-  //!!!!! code to  save the content in history
-  //
+    if (data.error) {
+        // Handle the error here
+        const errorMessage = JSON.stringify(data);
+        addMessage(errorMessage, 'error','../static/Error.png');
+    } else {
+        // Process the normal response here
+        const responseMessage = data['response'];
+        addMessage(responseMessage, 'aibot','../static/Bot_logo.png');
+    }
 };
-//
 
 //Section: Button to submit to the model and get the response
 messageForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const message = messageInput.value.trim();
-  if (message !== '') {
-    messageInput.value = '';
-    await sendMessage(message);
-  }
+    event.preventDefault();
+    const message = messageInput.value.trim();
+    if (message !== '') {
+        messageInput.value = '';
+        await sendMessage(message);
+    }
 });
